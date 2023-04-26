72599eacacfde5a824cd4d77270aeade85f0f722
Rohit Kumar Verma <rverma@aakashinformatics.com>
2023-04-26 15:20:17 +0530

    Add Ticket

---
+++ CRMS.Core/Models/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMS.Core.Models
+{
+    public class Ticket : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Guid AssignTo { get; set; }
+        public Guid TypeId { get; set; }
+        public Guid PriorityId { get; set; }
+        public Guid StatusId {get;set;}
+
+    }
+}
---
+++ CRMS.Core/Models/TicketStatusHistory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMS.Core.Models
+{
+    public class TicketStatusHistory : BaseEntity
+    {
+        public Guid TitleId { get; set; }
+        public string NewStatus { get; set; }
+        public string OldStatus { get; set; }        
+    }
+}
